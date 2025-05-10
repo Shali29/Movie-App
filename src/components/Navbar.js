327e644eb769d8d3aac3632a81ad38ff58027bc4
Shali29 <shalikaramanayaka21@gmail.com>
2025-05-10 23:37:21 +0530

    modify navbar for login and logout process

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,26 @@
 import React, { useContext } from 'react';
-import { AppBar, Toolbar, Typography, Button, TextField, InputAdornment, IconButton } from '@mui/material';
+import { 
+  AppBar, 
+  Toolbar, 
+  Typography, 
+  Button, 
+  TextField, 
+  InputAdornment, 
+  IconButton,
+  Box
+} from '@mui/material';
 import { MovieContext } from '../context/MovieContext';
 import { ThemeContext } from '../context/ThemeContext';
+import { AuthContext } from '../context/AuthContext';
 import SearchIcon from '@mui/icons-material/Search';
 import Brightness4Icon from '@mui/icons-material/Brightness4';
 import Brightness7Icon from '@mui/icons-material/Brightness7';
+import LogoutIcon from '@mui/icons-material/Logout';
 
 const Navbar = () => {
   const { search } = useContext(MovieContext);
   const { darkMode, toggleTheme } = useContext(ThemeContext);
+  const { isAuthenticated, logout } = useContext(AuthContext);
   const [searchQuery, setSearchQuery] = React.useState('');
 
   const handleSearch = (e) => {
@@ -23,25 +35,35 @@ const Navbar = () => {
           Movie Explorer
         </Typography>
         
-        <form onSubmit={handleSearch}>
-          <TextField
-            size="small"
-            value={searchQuery}
-            onChange={(e) => setSearchQuery(e.target.value)}
-            placeholder="Search movies..."
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SearchIcon />
-                </InputAdornment>
-              ),
-            }}
-          />
-        </form>
+        {isAuthenticated && (
+          <form onSubmit={handleSearch}>
+            <TextField
+              size="small"
+              value={searchQuery}
+              onChange={(e) => setSearchQuery(e.target.value)}
+              placeholder="Search movies..."
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <SearchIcon />
+                  </InputAdornment>
+                ),
+              }}
+            />
+          </form>
+        )}
         
-        <IconButton onClick={toggleTheme} color="inherit">
-          {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
-        </IconButton>
+        <Box sx={{ display: 'flex', alignItems: 'center', ml: 2 }}>
+          <IconButton onClick={toggleTheme} color="inherit">
+            {darkMode ? <Brightness7Icon /> : <Brightness4Icon />}
+          </IconButton>
+          
+          {isAuthenticated && (
+            <IconButton onClick={logout} color="inherit">
+              <LogoutIcon />
+            </IconButton>
+          )}
+        </Box>
       </Toolbar>
     </AppBar>
   );
